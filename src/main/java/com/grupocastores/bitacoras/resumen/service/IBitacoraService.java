dc937d45e09c4ad21a446d36a81f69dfb4ce4d6b
Oscar Guerra <desarrolloti32_lem@castores.com.mx>
2022-12-06 11:26:32 -0600

    se agrega comentarios en metodos

--- src/main/java/com/grupocastores/bitacoras/resumen/service/IBitacoraService.java
+++ src/main/java/com/grupocastores/bitacoras/resumen/service/IBitacoraService.java
@@ -6,18 +6,25 @@ import com.grupocastores.commons.inhouse.BitacoraResumenViajesCustom;
 import com.grupocastores.commons.inhouse.BitacoraResumenViajesDetail;
 
 
-//import castores.dao.talones.GuiaviajeDao;
-
 public interface IBitacoraService {
-
+    
+    /**
+     * filterViajes: Servicio para filtrar viajes.
+     * 
+     * @version 0.0.1
+     * @author Oscar Eduardo Guerra Salcedo [OscarGuerra]
+     * @date 2022-12-06
+     */
     public List<BitacoraResumenViajesCustom> filterViajes(String fechaInicio, String fechaFin, String idViaje, String noEconomico, int tipoUnidad,
             int estatusViaje, int idEsquema, int idNegociacion, int idClienteinhouse,String idOficinaCliente, String idoficinaDocumenta);
-
+    
+    /**
+     * getDetalleViaje: Servicio para obtener el detalle del resumen de viaje.
+     * 
+     * @version 0.0.1
+     * @author Oscar Eduardo Guerra Salcedo [OscarGuerra]
+     * @date 2022-12-06
+     */
     public BitacoraResumenViajesDetail getDetalleViaje(int idNegociacion, int idEsquemaViaje, int idRuta,
-            int idCliente, String idOficinaCliente, 
-
-            
-            
-            
-            String idoficinaDocumenta, int idUnidad, int noEconomico);
+            int idCliente, String idOficinaCliente, String idoficinaDocumenta, int idUnidad, int noEconomico);
     }
